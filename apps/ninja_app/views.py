c4d72190e7d0db236727b6993859f4be54c39b70
Placoderm <placoderm@Sarah-MacBook-Air.local>
2017-05-23 16:11:10 -0700

    cleaned up comments

--- apps/ninja_app/views.py
+++ apps/ninja_app/views.py
@@ -5,15 +5,21 @@ from django.shortcuts import render, redirect
 from random import randrange
 
 # Create your views here.
+
+#shows ws the main index page
 def index(request):
+    #initialize gold amount and activities array in session
     if 'gold' not in request.session:
         request.session['gold'] = 0
     if 'activities' not in request.session:
         request.session['activities'] = []
     return render(request, 'ninja_app/index.html')
 
+#called when user clicks one of the submit forms on main page
 def process(request):
     if request.method == "POST":
+        #depending on what they clicked
+        #user receives different amounts of gold
         if request.POST['building'] == "farm":
             money = randrange(10,21)
         elif request.POST['building'] == "cave":
@@ -30,6 +36,8 @@ def process(request):
         else:
             sign = -1
         money = abs(money)
+        #add message to session activities array
+        #which will be printed on main page
         request.session['activities'].append((sign, money, building))
         print request.session['activities']
     return redirect('/')
